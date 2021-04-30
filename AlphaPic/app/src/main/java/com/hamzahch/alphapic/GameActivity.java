966c08819f7a2e3f5071c10bd375239d2421a1a4
Hamzah Chaudhry <haz540@gmail.com>
2021-04-30 03:04:09 -0400

    Implemented timer and part 2 progress

--- AlphaPic/app/src/main/java/com/hamzahch/alphapic/GameActivity.java
+++ AlphaPic/app/src/main/java/com/hamzahch/alphapic/GameActivity.java
@@ -2,8 +2,14 @@ package com.hamzahch.alphapic;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.SoundPool;
+import android.graphics.Bitmap;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
 
 import java.util.Random;
@@ -12,12 +18,15 @@ public class GameActivity extends Activity {
 
     private int[] imgResources;
     private int[] soundResources;
-    private SoundPool mSoundPool;
+    private MediaPlayer mPlayer;
+    private Chronometer mTimer;
+    private ImageButton mCurrBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        mTimer = findViewById(R.id.stopwatch);
 
         // define boxes and sounds
         imgResources = new int[] { R.id.boxA, R.id.boxB, R.id.boxC, R.id.boxD, R.id.boxE, R.id.boxF, R.id.boxG,
@@ -28,18 +37,30 @@ public class GameActivity extends Activity {
             R.raw.h, R.raw.i, R.raw.j, R.raw.k, R.raw.l, R.raw.m, R.raw.n, R.raw.o, R.raw.p, R.raw.q,
             R.raw.r, R.raw.s, R.raw.t, R.raw.u, R.raw.v, R.raw.w, R.raw.x, R.raw.y, R.raw.z };
 
+        // make boxes unclickable
+        for (int res : imgResources) {
+            ImageButton btn = findViewById(res);
+            btn.setClickable(false);
+        }
+
         // start background music
         Intent i = new Intent(this, MusicService.class);
         startService(i);
 
-        // select random box
-        Random r = new Random();
-        int box = r.nextInt(imgResources.length);
-        ImageButton img = findViewById(imgResources[box]);
-        img.setBackgroundResource(R.drawable.image_border_active);
-        mSoundPool = new SoundPool.Builder().build();
-        int sound = mSoundPool.load(this, soundResources[box], 1);
-        mSoundPool.play(sound, 1, 1, 0, 0, 1);
+        // start new round
+        startRound();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap img = (Bitmap) extras.get("data");
+            mCurrBox.setImageBitmap(img);
+            stopRound();
+        }
     }
 
     @Override
@@ -49,6 +70,39 @@ public class GameActivity extends Activity {
         // stop background music
         Intent i = new Intent(this, MusicService.class);
         stopService(i);
-        mSoundPool.release();
+        mPlayer.release();
     }
+
+    public void onBoxClick(View view) {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 1);
+    }
+
+    public void onExit(View view) {
+        finish();
+    }
+
+    public void startRound() {
+        // start timer
+        mTimer.start();
+
+        // select random box
+        Random r = new Random();
+        int box = r.nextInt(imgResources.length);
+        mCurrBox = findViewById(imgResources[box]);
+        mCurrBox.setBackgroundResource(R.drawable.image_border_active);
+        mCurrBox.setClickable(true);
+        mPlayer = MediaPlayer.create(this, soundResources[box]);
+        mPlayer.start();
+    }
+
+    public void stopRound() {
+        // reset
+        mTimer.setBase(SystemClock.elapsedRealtime());
+        mTimer.stop();
+        mCurrBox.setClickable(false);
+        mCurrBox.setBackgroundResource(R.drawable.image_border);
+        startRound();
+    }
+
 }
